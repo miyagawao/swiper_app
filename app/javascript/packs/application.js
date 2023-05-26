e1528ca0e44ebb4e0a4fa92e0802a2abc799e4e7
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-05-26 08:16:11 +0000

    Add swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import "script.js"
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
